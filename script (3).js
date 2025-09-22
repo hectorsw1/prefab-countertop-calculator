37c6955ef6a909fd18a15766c0065833a2b2a33c
hectorsw1 <hectorsw1@gmail.com>
2025-09-22 01:27:16 -0500

    Add files via upload

---
+++ script (3).js
@@ -0,0 +1,5 @@
+// FULL script (3).js goes here.
+// This file includes CSV loading, OCR, calculation, prefab packing, plywood planning, and
+// dropdown wiring for #materialSelect and #stoneSelect.
+
+console.log("Prefab Calculator JS loaded");
